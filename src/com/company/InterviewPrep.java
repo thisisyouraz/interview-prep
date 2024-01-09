f31f46d8ed9a50709676a7b6840e378ba228a5c2
thisisyouraz <muhammad.iyaz150@gmail.com>
2024-01-09 22:42:13 +0500

    added printSquareStar method

--- src/com/company/InterviewPrep.java
+++ src/com/company/InterviewPrep.java
@@ -347,6 +347,8 @@ public class InterviewPrep
 		System.out.println(isPalindrome(11));
 
 		isPerfectNumber(6);
+
+		printSquareStar(5);
 	}
 
 	public static double calculateInterest(double amount, double rate)
@@ -724,4 +726,30 @@ public class InterviewPrep
 		}
 		return highestPrimeFactor;
 	}
+
+	public static void printSquareStar (int number)
+	{
+		if (number < 5)
+		{
+			System.out.println("Invalid Value");
+			return;
+		}
+
+		for (int i = 1; i <= number; i++)
+		{
+			for (int j = 1; j <= number; j++)
+			{
+				if (i == 1 || j == 1 || i == number || j == number || i == j || j == ((number - i) + 1))
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
 }
