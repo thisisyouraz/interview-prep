52adc86959d883182e5ce9af92c48fd84143d14e
thisisyouraz <muhammad.iyaz150@gmail.com>
2024-01-25 00:42:57 +0500

    SOLID deign principal.

---
+++ src/com/learning/designPatterns/DesignPattern.java
@@ -0,0 +1,26 @@
+package com.learning.designPatterns;
+
+public class DesignPattern
+{
+	/*
+	SOLID : is an acronym that represents the five most important design principals in creating a flexible, more understandable
+	and maintainable code.
+
+		1 - Single Responsibility principle.
+		2 - Open close principle.
+		3 - Liskov substitution principle.
+		4 - Interface segregation principle.
+		5 - Dependency inversion principle.
+
+		Single responsibility principle:
+			There should be only one reason to change a class. This suggests that one class should only be focused
+			on one functionality.
+
+		Open close principal:
+			software entities (Class, methods, modules) should be open for extension but close for modification.
+			which means in order to add new functionality to a software one should be able to achieve that without
+			modifying the already written and tested code. (This principal based on inheritance)
+			- Base classes are generally closed for modification and child classes can be modified or one can
+			  create a new child class by extending the base class.
+	 */
+}
